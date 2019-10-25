e13a3f76dbe372df95357aa3bfd5978de3ea496a
sawpaio <lucas.msampaio@outlook.com>
2019-10-25 09:24:16 -0300

    Add files via upload

---
+++ csvtojson2.py
@@ -0,0 +1,17 @@
+import csv
+import json
+from pprint import pprint
+from datetime import date
+
+f = open('join_fin.csv', 'r')
+data_atual = date.today()
+a = '"index": {}}'
+reader = csv.DictReader(f, fieldnames = ("code","name","priceUSD","change24H","change7D","symbol","priceBTC","priceReal","marketCap","volume24H","totalVolume"))
+jsonfile = open('processado_{}.json'.format(data_atual), 'w')
+
+for row in reader:
+    json.dump({"index":{}}, jsonfile)
+    jsonfile.write('\n')
+    json.dump(row, jsonfile)
+    jsonfile.write('\n')
+
